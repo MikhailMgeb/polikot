3a7f2b07066d1dee0779948e36993d2b14fbfb63
Michael Mgebrishvili <mmd920802@gmail.com>
2024-02-04 11:07:01 +0300

    fix bag

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
 const cors = require('cors');
 const port = 3000;
+
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/translate.json');
+    const language = req.query.language;
+    const dataBase = fs.readFileSync(path.join(__dirname, 'translate.json'), 'utf-8');
+    const dataBaseObject = JSON.parse(dataBase);
+
+    const answerServer = {
+        language: dataBaseObject[language]
+    };
+
+    res.json(answerServer);
 })
 
 app.listen(port, () => {
